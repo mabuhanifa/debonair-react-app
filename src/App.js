c2f8984c9bdbcca56178fce4e9812eacec5cd4fd
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-13 21:51:04 +0600

    tabs outside of route

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Tab, Tabs } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Employees from "./components/Employees";
 import Home from "./components/Home";
@@ -5,11 +6,17 @@ import Users from "./components/Users";
 
 function App() {
   return (
+    <>
+    <Tabs>
+      <Tab label="User" components={Users} />
+      <Tab label="Employees" to="/employees" />
+    </Tabs>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/users" element={<Users />} />
       <Route path="/employees" element={<Employees />} />
     </Routes>
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,9 @@
-import { Tab, Tabs } from "@mui/material";
 import React from "react";
 
 export default function Home() {
   return (
-    <Tabs>
-      <Tab label="User" href="users"/>
-      <Tab label="Employees" to="/employees" />
-    </Tabs>
+    <>
+     
+    </>
   );
 }
