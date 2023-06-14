81528fb9f255133acf74c11c7e1219c2928454c8
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-14 15:25:11 +0600

    employees fetched

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Tab, Tabs } from "@mui/material";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Employees from "./components/Employees";
 import Home from "./components/Home";
 import Users from "./components/Users";
@@ -7,15 +7,15 @@ import Users from "./components/Users";
 function App() {
   return (
     <>
-    <Tabs>
-      <Tab label="User" components={Users} />
-      <Tab label="Employees" to="/employees" />
-    </Tabs>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/users" element={<Users />} />
-      <Route path="/employees" element={<Employees />} />
-    </Routes>
+      <Tabs>
+        <Tab label="User" component={Link} to="/users" value={0}/>
+        <Tab label="Employees" component={Link} to="/employees" value={1}/>
+      </Tabs>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/employees" element={<Employees />} />
+      </Routes>
     </>
   );
 }
--- src/components/Employees.js
+++ src/components/Employees.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Employees() {
-  return (
-    <div>Employees</div>
-  )
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      const res = await fetch(
+        "http://59.152.62.177:8085/api/Employee/EmployeeData"
+      );
+      const data = await res.json();
+      setEmployees(data.readEmployeeData);
+    };
+    fetchEmployees();
+  }, []);
+  console.log(employees);
+  return <div>Employees</div>;
 }
