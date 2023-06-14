d14a8dd62a6056f9d1463a5fa5b9410802e419e0
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-14 21:19:03 +0600

    table rendered with data

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,3 +1,13 @@
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 export default function Users() {
@@ -9,10 +19,39 @@ export default function Users() {
         "http://59.152.62.177:8085/api/Employee/EmployeeData"
       );
       const data = await res.json();
-      
-      console.log(data);
+
+      setUsers(data.readEmployeeData);
     };
     fetchUsers();
   }, []);
-  return <div>Users</div>;
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Employee Type</TableCell>
+              <TableCell>Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.map((user) => (
+              <TableRow key={user.empID}>
+                <TableCell>{user.empID}</TableCell>
+                <TableCell>{user.firstName}</TableCell>
+                <TableCell>{user.lastName}</TableCell>
+                <TableCell>{user.employeeType}</TableCell>
+                <TableCell>
+                  <Button variant="contained">Details</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
